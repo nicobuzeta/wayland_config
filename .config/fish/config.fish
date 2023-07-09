021d2754d9748bf85328fc18a25eba3ffdb82b5c
Nicolas Buzeta <nicolas@buzeta.net>
2023-07-09 14:19:25 -0400

    Add nvidia sway to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+if test -z "$WAYLAND_DISPLAY" ; test "$XDG_VTNR" -eq 1
+    sway-nvidia
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
@@ -5,4 +9,4 @@ end
 
 set fish_greeting ""
 load_nvm
-alias config='/usr/bin/git --git-dir=$HOME/wayland_config/.git/ --work-tree=$HOME'
+alias config='/usr/bin/git --git-dir=$HOME/wayland_config --work-tree=$HOME'
